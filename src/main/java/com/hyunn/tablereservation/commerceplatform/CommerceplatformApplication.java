46bee00b21ab02518dee07f4fd1f9fd89fad8a3b
Hyun Cho <skfkzpql@gmail.com>
2024-06-23 08:04:07 +0900

    Initialize project

---
+++ src/main/java/com/hyunn/tablereservation/commerceplatform/CommerceplatformApplication.java
@@ -0,0 +1,12 @@
+package com.hyunn.tablereservation.commerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceplatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommerceplatformApplication.class, args);
+  }
+}
---
+++ src/test/java/com/hyunn/tablereservation/commerceplatform/CommerceplatformApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hyunn.tablereservation.commerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceplatformApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
